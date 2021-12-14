38d4310da8f2311cd252da39e6bb63cb3bcb1fd1
Giuseppe Mattia Greco <giumatt99@gmail.com>
2021-12-14 11:50:45 +0100

    Fixed script for LightDM.

--- scripts/light_monitor.sh
+++ scripts/light_monitor.sh
@@ -3,14 +3,12 @@
 # This script check if the monitor is plugged and change the settings on LightDM
 # conf file to match the screens configuration.
 
-x=$(xrandr)
-
-if echo "$x" | grep "HDMI-A-0 disconnected"; then
+if $1 = "monitor"; then
   sed -i '/display-setup-script=xrandr --output HDMI-A-0 --primary --mode 1920x1080/s/^/#/g' /etc/lightdm/lightdm.conf
   sed -i '/active-monitor = eDP/s/^#//g' /etc/lightdm/lightdm-gtk-greeter.conf
   sed -i '/active-monitor = HDMI-A-0/s/^/#/g' /etc/lightdm/lightdm-gtk-greeter.conf
   systemctl restart lightdm
-else
+elif $1 = "HDMI" || $1 = "hdmi"; then
   sed -i '/display-setup-script=xrandr --output HDMI-A-0 --primary --mode 1920x1080/s/^#//g' /etc/lightdm/lightdm.conf
   sed -i '/active-monitor = eDP/s/^/#/g' /etc/lightdm/lightdm-gtk-greeter.conf
   sed -i '/active-monitor = HDMI-A-0/s/^#//g' /etc/lightdm/lightdm-gtk-greeter.conf
