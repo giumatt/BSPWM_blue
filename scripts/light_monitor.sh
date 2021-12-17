b555ff002b7fa17fd7b4d23c840f368900b96eaa
Giuseppe Mattia Greco <giumatt99@gmail.com>
2021-12-17 16:23:58 +0100

    Fixed script for LightDM and .gitignore

--- scripts/light_monitor.sh
+++ scripts/light_monitor.sh
@@ -3,12 +3,12 @@
 # This script check if the monitor is plugged and change the settings on LightDM
 # conf file to match the screens configuration.
 
-if $1 = "monitor"; then
+if [ "$1" = "monitor" ]; then
   sed -i '/display-setup-script=xrandr --output HDMI-A-0 --primary --mode 1920x1080/s/^/#/g' /etc/lightdm/lightdm.conf
   sed -i '/active-monitor = eDP/s/^#//g' /etc/lightdm/lightdm-gtk-greeter.conf
   sed -i '/active-monitor = HDMI-A-0/s/^/#/g' /etc/lightdm/lightdm-gtk-greeter.conf
   systemctl restart lightdm
-elif $1 = "HDMI" || $1 = "hdmi"; then
+elif [ "$1" = "HDMI" || $1 = "hdmi" ]; then
   sed -i '/display-setup-script=xrandr --output HDMI-A-0 --primary --mode 1920x1080/s/^#//g' /etc/lightdm/lightdm.conf
   sed -i '/active-monitor = eDP/s/^/#/g' /etc/lightdm/lightdm-gtk-greeter.conf
   sed -i '/active-monitor = HDMI-A-0/s/^#//g' /etc/lightdm/lightdm-gtk-greeter.conf
